5f96e00dc3e669940b504cd6d4b6eb39376733ad
caionikolas <Caio.kanu@hotmail.com>
2022-06-15 01:16:39 -0300

    mobile

--- main.js
+++ main.js
@@ -25,3 +25,30 @@ window.addEventListener("scroll", function() {
     header.classList.remove("scroll")
   }
 })
+
+/* Testimonials Carrossel slider Swiper */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* scrollReveal: Mostrar elementos quando dê scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `, 
+  { interval: 100 })
