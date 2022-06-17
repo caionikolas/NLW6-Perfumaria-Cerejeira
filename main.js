fa25a0116d8a812308a2669c2369cdad18272efe
caionikolas <Caio.kanu@hotmail.com>
2022-06-16 21:21:24 -0300

    responsividade

--- main.js
+++ main.js
@@ -15,16 +15,20 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando de scroll */
-const header = document.querySelector("header")
-const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function() {
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("header")
+  const navHeight = header.offsetHeight;
+
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll")
   } else {
     header.classList.remove("scroll")
   }
-})
+}
+
+
 
 /* Testimonials Carrossel slider Swiper */
 const swiper = new Swiper('.swiper', {
@@ -49,6 +53,24 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, 
   { interval: 100 })
+
+  /* botão voltar para o topo */
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  function backToTop() {
+    if(window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  }
+
+  /* When Scroll */
+  window.addEventListener("scroll", function() {
+    changeHeaderWhenScroll()
+    backToTop()
+  })
